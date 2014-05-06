fc0a2c442c682820850ba388400045480b61c4df
Gueust <jeanbaptiste.lespiau@gmail.com>
2014-05-06 09:18:05 +0200

    Completed the grammar
    - It now accepts A-Z and _ in identifiers
    - Created the backbone of the parsing of formula within Formula.java

--- BooleanExpression.g4
+++ BooleanExpression.g4
@@ -3,22 +3,51 @@ grammar BooleanExpression;
 @lexer::header {
 package abstractGraph.Conditions.parser;
 }
- 
+
 @parser::header {
 package abstractGraph.Conditions.parser;
 }
 
 /* Putting AND before the OR operator enforces the priority of AND versus OR */
-booleanExpression : booleanExpression AND booleanExpression 
-          |booleanExpression OR booleanExpression       
-          |'(' booleanExpression ')'
-          | expression;
-          
-expression : ID ;
- 
+booleanExpression
+:
+  booleanExpression AND booleanExpression
+  | booleanExpression OR booleanExpression
+  | '(' booleanExpression ')'
+  | expression
+;
+
+expression
+:
+  ID
+;
+
 //Tokens
-ID : [a-z|0-9]+ ;
-AND : 'AND' | 'ET' |'&&' | '&' ;
-OR : 'OR' | 'OU' | '||' | '|' ;
+
+AND
+:
+  'AND'
+  | 'ET'
+  | '&&'
+  | '&'
+;
+
+OR
+:
+  'OR'
+  | 'OU'
+  | '||'
+  | '|'
+;
+
+ID
+:
+  [a-zA-Z0-9_]+
+;
+
 //Specify to ignore the whitespace and new line character in the parsed file.
-WS : [ \t\r\n]+ -> skip ;
+
+WS
+:
+  [ \t\r\n]+ -> skip
+;
--- src/abstractGraph/Conditions/Formula.java
+++ src/abstractGraph/Conditions/Formula.java
@@ -1,5 +1,37 @@
 package abstractGraph.Conditions;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import abstractGraph.Conditions.parser.BooleanExpressionLexer;
+import abstractGraph.Conditions.parser.BooleanExpressionParser;
+
 public abstract class Formula {
 
+  /**
+   * Parse a string expression into a formula.
+   * The formula accepts AND, &&, &, ET as the "and" operator ;
+   * OR, ||, |, OU as the "or" operator".
+   * 
+   * @param expression
+   *          The string to parse
+   * @return The exact formula parsed without any modification.
+   */
+  static public Formula parse(String expression) {
+    ANTLRInputStream input = new ANTLRInputStream(expression);
+
+    /* Create a lexer that feeds off of input CharStream */
+    BooleanExpressionLexer lexer = new BooleanExpressionLexer(input);
+
+    /* Create a buffer of tokens pulled from the lexer */
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+    /* Create a parser that feeds off the tokens buffer */
+    BooleanExpressionParser parser = new BooleanExpressionParser(tokens);
+    /* begin parsing at booleanExpression rule */
+    ParseTree tree = parser.booleanExpression();
+
+    return null;
+  }
 }
