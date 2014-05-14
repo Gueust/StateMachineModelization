44064fabc6ca992e9b2ba96996533a8a605b895c
Gueust <jeanbaptiste.lespiau@gmail.com>
2014-05-14 10:29:20 +0200

    Comment on the verbose parameter of the check() fonction.

--- src/graph/verifiers/AbstractVerificationUnit.java
+++ src/graph/verifiers/AbstractVerificationUnit.java
@@ -10,6 +10,16 @@ public abstract class AbstractVerificationUnit {
    * @param m
    *          The model on which to apply the verification.
    * @param verbose
+   *          Prints the status of the checking before returning the boolean
+   *          result.
+   *          It is equivalent to:
+   *          <p><blockquote><pre>
+   *          if (check(m, false)) {
+   *          System.out.println(successMessage());
+   *          } else {
+   *          System.out.println(errorMessage());
+   *          }
+   *          </pre></blockquote><p>
    * @return True if the verification succeed.
    */
   abstract public boolean check(Model m, boolean verbose);
