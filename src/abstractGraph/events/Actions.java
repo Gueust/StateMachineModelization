f66976a9fc0d8d7bbb9df15521156eb3597e5d53
Gueust <jeanbaptiste.lespiau@gmail.com>
2014-05-19 13:01:39 +0200

    Forgotten commit: equals has been added into Actions.java

--- src/abstractGraph/events/Actions.java
+++ src/abstractGraph/events/Actions.java
@@ -25,4 +25,34 @@ public class Actions {
     }
     return sb.toString();
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Actions other = (Actions) obj;
+    if (events == null) {
+      if (other.events != null) {
+        return false;
+      }
+    }
+
+    if (this.events.size() != other.events.size()) {
+      return false;
+    }
+
+    for (int i = 0; i < events.size(); i++) {
+      if (!events.get(i).toString().equals(other.events.get(i).toString())) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
