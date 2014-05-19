00f62b13113549b649c843abe4ddf3ce6bd43b78
Nassim Wizz <sim.moussi@gmail.com>
2014-05-19 17:31:17 +0200

    Implementing the functions of AbstractGlobalStates. Implementing the function evalCondtion of the class Transition

--- src/abstractGraph/AbstractGlobalState.java
+++ src/abstractGraph/AbstractGlobalState.java
@@ -1,9 +1,18 @@
 package abstractGraph;
 
+import graph.State;
+import graph.StateMachine;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
 import abstractGraph.conditions.Variable;
 
-public interface AbstractGlobalState<M extends AbstractStateMachine<S, T>, S extends AbstractState<T>, T extends AbstractTransition<S>> {
+public abstract class AbstractGlobalState<M extends AbstractStateMachine<S, T>, S extends AbstractState<T>, T extends AbstractTransition<S>> {
 
+  private HashMap<Variable, Boolean> variables = new HashMap<Variable, Boolean>();
+  private HashMap<StateMachine, State> state_machines_tag =
+      new HashMap<StateMachine, State>();
   /**
    * Set the active state of `machine` to S.
    * 
@@ -12,11 +21,17 @@ public interface AbstractGlobalState<M extends AbstractStateMachine<S, T>, S ext
    * @param S
    *          The state to set.
    */
-  public abstract void setState(M machine, S state);
+  public void setState(StateMachine machine, State state) {
+    state_machines_tag.put(machine, state);
+  }
 
-  public abstract S getState(M machine);
+  public State getState(StateMachine machine) {
+    return state_machines_tag.get(machine);
+  }
 
-  public abstract boolean getVariableValue(Variable variable);
+  public boolean getVariableValue(Variable variable) {
+    return variables.get(variable);
+  }
 
   /**
    * Change the value of the variable.
@@ -26,6 +41,16 @@ public interface AbstractGlobalState<M extends AbstractStateMachine<S, T>, S ext
    *          the new value to assign
    * @return true if the variable changed it's value, false otherwise.
    */
-  public abstract boolean setVariableValue(Variable variable, boolean value);
+  public boolean setVariableValue(Variable variable, boolean value) {
+    if (variables.get(variable) != value) {
+      variables.put(variable, value);
+      return true;
+    }
+    return false;
+  }
+  
+  public Iterator<Variable> iteratorVariables(){
+    return variables.keySet().iterator();
+  }
 
 }
