3aa57724fa8751ea36cfbc1b383240cd25afcd60
Gueust <jeanbaptiste.lespiau@gmail.com>
2014-06-10 09:02:13 +0200

    Forgotten the Pair class.

---
+++ src/utils/Pair.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class Pair<E, F> {
+  public E first;
+  public F second;
+
+  public Pair(E first, F second) {
+    this.first = first;
+    this.second = second;
+  }
+
+  public E getFirst() {
+    return first;
+  }
+
+  public F getSecond() {
+    return second;
+  }
+}
