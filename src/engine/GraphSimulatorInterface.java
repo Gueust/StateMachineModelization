0c238e9dfd0d324605d6f02d2814125855858ae7
Gueust <jeanbaptiste.lespiau@gmail.com>
2014-06-13 13:27:35 +0200

    public execute functions in simulators now MUST return a new instance

--- src/engine/GraphSimulatorInterface.java
+++ src/engine/GraphSimulatorInterface.java
@@ -8,28 +8,24 @@ import abstractGraph.events.ExternalEvent;
 
 public interface GraphSimulatorInterface<GS extends AbstractGlobalState<M, S, T>, M extends AbstractStateMachine<S, T>, S extends AbstractState<T>, T extends AbstractTransition<S>> {
 
+  public GraphSimulatorInterface<GS, M, S, T> clone();
+
   /**
    * Execute one external event completely.
-   * It does modify the GlobalState given as parameter.
+   * It MUST NOT modify the GlobalState given as parameter.
    * 
    * @param e
    *          An external event to execute.
-   * @param global_state
+   * @param starting_state
    *          The global state on which to run the event.
-   * @return A global state being the result of the execution. It is the same as
-   *         the parameter.
+   * @return A global state being the result of the execution.
    */
   public GS execute(GS starting_state, ExternalEvent e);
 
   /**
    * {@inheritDoc #execute(AbstractGlobalState, ExternalEvent)} Same as
-   * {@link #executeOnlyFunctional(AbstractGlobalState, ExternalEvent)} using the
-   * GlobalState contained in the GraphSimulator instance.
-   * 
-   * It does modify the internal GlobalState, meaning that several calls to this
-   * function will always return the same pointer to the same GlobalState.
-   * 
-   * @return The internal GlobalState, that has been modified by the execution.
+   * {@link #executeOnlyFunctional(AbstractGlobalState, ExternalEvent)} using
+   * the GlobalState contained in the GraphSimulator instance.
    */
   public GS execute(ExternalEvent e);
 }
