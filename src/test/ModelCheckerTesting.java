17344c423c4be74ecab0eb47dc5a71c87d918592
Nassim Wizz <sim.moussi@gmail.com>
2014-06-18 09:57:44 +0200

    Added a testing class for ModelChecker.

---
+++ src/test/ModelCheckerTesting.java
@@ -0,0 +1,94 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import engine.GraphSimulator;
+import engine.ModelChecker;
+import graph.GlobalState;
+import graph.GraphFactoryAEFD;
+import graph.Model;
+import graph.State;
+import graph.StateMachine;
+import graph.Transition;
+import graph.templates.GeneratorFromTemplate;
+import graph.verifiers.SingleWritingChecker;
+import graph.verifiers.Verifier;
+
+public class ModelCheckerTesting {
+
+  private void generalTest(String[] files, int[] results) throws IOException {
+    assertTrue(files.length == results.length);
+
+    int i = 0;
+    ModelChecker<GlobalState, StateMachine, State, Transition> model_checker;
+    for (i = 0; i < files.length; i++) {
+      model_checker = new ModelChecker<>();
+      GraphSimulator simulator = generateSimulator(files[i], null);
+      model_checker.configureInitialGlobalStates(simulator
+          .getAllInitialStates());
+      model_checker.verify(simulator);
+
+      System.err.print(model_checker.getVisited_states());
+      int number_visited_states = model_checker.getVisited_states().size();
+
+      assertTrue("Error on " + files[i] +
+          " (" + number_visited_states + " found.  " + results[i]
+          + " expected).\n",
+          model_checker.getVisited_states().size() == results[i]);
+    }
+  }
+
+  @Test
+  public void modelCheckerTesting() throws IOException {
+    Verifier verifier = new Verifier();
+    verifier.addVerification(new SingleWritingChecker());
+
+    String[] files = {
+        "Three_ctl.yaml",
+        "Eight_ctl.yaml",
+        // "Twelve_ctl.yaml,"
+        "graph_with_dependency.yaml"
+    };
+
+    int[] results = {
+        8,
+        256,
+        // 1 << 12,
+        3
+    };
+
+    generalTest(files, results);
+  }
+
+  public GraphSimulator generateSimulator(String model_file, String proof_file)
+      throws IOException {
+    String functional_model = GeneratorFromTemplate
+        .load("src/test/resources/" + this.getClass().getSimpleName() + "/"
+            + model_file);
+    String proof_model;
+    Model model;
+    GraphSimulator simulator;
+    GraphFactoryAEFD graph_factory = new GraphFactoryAEFD();
+    model = graph_factory
+        .buildModel(functional_model, functional_model);
+    model.build();
+    if (proof_file != null) {
+      proof_model = GeneratorFromTemplate
+          .load("src/test/resources/" + this.getClass().getSimpleName() + "/"
+              + proof_file);
+      Model proof = graph_factory.buildModel(proof_model, proof_model);
+      proof.build();
+      simulator = new GraphSimulator(model, proof);
+    } else {
+      simulator = new GraphSimulator(model);
+    }
+    simulator.setVerbose(false);
+    return simulator;
+
+  }
+
+}
