45145ed17705e569264567bd53ebb8a1bca4f999
Gueust <jeanbaptiste.lespiau@gmail.com>
2014-08-19 09:06:00 +0200

    Added a JScrollPane in the StateDisplayer

--- src/engine/traceTree/StateDisplayer.java
+++ src/engine/traceTree/StateDisplayer.java
@@ -17,6 +17,7 @@ import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
 import engine.GraphSimulatorInterface;
+import javax.swing.JScrollPane;
 
 /**
  * A window that display the content of the clicked state
@@ -34,37 +35,45 @@ public class StateDisplayer<GS extends AbstractGlobalState<M, S, T, ?>, M extend
 
     simulator = graph_simulator;
 
-    textPane = new JTextPane();
-    textPane.setEditable(false);
-
     btnNewButton = new JButton("Get the same state with children");
+
+    JScrollPane scrollPane = new JScrollPane();
     GroupLayout groupLayout = new GroupLayout(getContentPane());
     groupLayout.setHorizontalGroup(
         groupLayout.createParallelGroup(Alignment.LEADING)
-            .addGroup(groupLayout
-                .createSequentialGroup()
-                .addGroup(groupLayout
-                    .createParallelGroup(Alignment.LEADING)
-                    .addComponent(btnNewButton)
-                    .addComponent(textPane, GroupLayout.DEFAULT_SIZE,
-                        384, Short.MAX_VALUE))
-                .addGap(0))
+            .addGroup(
+                groupLayout.createSequentialGroup()
+                    .addGroup(
+                        groupLayout.createParallelGroup(Alignment.LEADING)
+                            .addComponent(btnNewButton)
+                            .addGroup(
+                                groupLayout.createSequentialGroup()
+                                    .addGap(10)
+                                    .addComponent(scrollPane,
+                                        GroupLayout.DEFAULT_SIZE, 200,
+                                        Short.MAX_VALUE)))
+                    .addContainerGap())
         );
     groupLayout.setVerticalGroup(
         groupLayout.createParallelGroup(Alignment.LEADING)
-            .addGroup(groupLayout
-                .createSequentialGroup()
-                .addComponent(btnNewButton)
-                .addPreferredGap(ComponentPlacement.RELATED)
-                .addComponent(textPane, GroupLayout.DEFAULT_SIZE, 244,
-                    Short.MAX_VALUE)
-                .addGap(0))
+            .addGroup(
+                groupLayout.createSequentialGroup()
+                    .addComponent(btnNewButton)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 222,
+                        Short.MAX_VALUE)
+                    .addContainerGap())
         );
+
+    textPane = new JTextPane();
+    scrollPane.setViewportView(textPane);
+    textPane.setEditable(false);
     getContentPane().setLayout(groupLayout);
     setPreferredSize(new Dimension(400, 300));
     setAlwaysOnTop(true);
     setTitle("Display of the states. Click on a state in the tree to see its details here.");
     pack();
+    setLocationRelativeTo(null);
     setVisible(true);
 
   }
@@ -73,5 +82,4 @@ public class StateDisplayer<GS extends AbstractGlobalState<M, S, T, ?>, M extend
   public void setText(AbstractGlobalState<M, S, T, ?> state) {
     textPane.setText(simulator.globalStateToString((GS) state));
   }
-
 }
